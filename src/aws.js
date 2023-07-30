cbd359e74e5f83654d7d9166f4368281a0195008
aarnphm-ec2-dev <29749331+aarnphm@users.noreply.github.com>
2023-07-30 06:25:05 +0000

    chore: simplify a bit, remove runner-home-dir
    Signed-off-by: aarnphm-ec2-dev <29749331+aarnphm@users.noreply.github.com>

--- src/aws.js
+++ src/aws.js
@@ -4,30 +4,16 @@ const config = require('./config');
 
 // User data scripts are run as the root user
 function buildUserDataScript(githubRegistrationToken, label) {
-  if (config.input.runnerHomeDir) {
-    // If runner home directory is specified, we expect the actions-runner software (and dependencies)
-    // to be pre-installed in the AMI, so we simply cd into that directory and then start the runner
     return [
-      '#!/bin/bash',
-      'set -x',
-      `cd "${config.input.runnerHomeDir}"`,
-      'export RUNNER_ALLOW_RUNASROOT=1',
-      `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --pat ${githubRegistrationToken} --labels ${label} --unattended --ephemeral`,
-      './run.sh',
+        '#!/bin/bash',
+        'set -x',
+        'mkdir actions-runner && cd actions-runner',
+        'case $(uname -m) in aarch64) ARCH="arm64" ;; amd64|x86_64) ARCH="x64" ;; esac && export RUNNER_ARCH=${ARCH}',
+        'curl -O -L https://github.com/actions/runner/releases/download/v2.299.1/actions-runner-linux-${RUNNER_ARCH}-2.299.1.tar.gz',
+        'tar xzf ./actions-runner-linux-${RUNNER_ARCH}-2.299.1.tar.gz',
+        `RUNNER_ALLOW_RUNASROOT=1 ./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label} --unattended`,
+        'RUNNER_ALLOW_RUNASROOT=1 ./run.sh',
     ];
-  } else {
-    return [
-      '#!/bin/bash',
-      'set -x',
-      'mkdir actions-runner && cd actions-runner',
-      'case $(uname -m) in aarch64) ARCH="arm64" ;; amd64|x86_64) ARCH="x64" ;; esac && export RUNNER_ARCH=${ARCH}',
-      'curl -O -L https://github.com/actions/runner/releases/download/v2.307.1/actions-runner-linux-${RUNNER_ARCH}-2.307.1.tar.gz',
-      'tar xzf ./actions-runner-linux-${RUNNER_ARCH}-2.307.1.tar.gz',
-      'export RUNNER_ALLOW_RUNASROOT=1',
-      `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --pat ${githubRegistrationToken} --labels ${label} --unattended --ephemeral`,
-      './run.sh',
-    ];
-  }
 }
 
 async function startEc2Instance(label, githubRegistrationToken) {
